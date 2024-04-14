979c569cd28c1c424b11ffcd39e99d018d8082ca
SaccoGabriel <gabriel.sacco75@gmail.com>
2024-04-14 18:58:26 +0200

    Amélioration du design de toutes les sections, ajout section workState, corrections de bugs

---
+++ src/components/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import ParticlesComponent from "./particles.jsx";
+
+
+function App(){
+    return(
+        <div className="App">
+        <ParticlesComponent id ="#particles">yo la team</ParticlesComponent>
+        </div>
+    )
+}
+
+export default App;
